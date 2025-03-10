92d0a993ddda3473b5286bef4bae6b6db288f1ad
Bora06 <bora.arcak@gmail.com>
2025-03-09 20:06:06 -0500

    first

---
+++ calculator.py
@@ -0,0 +1,32 @@
+def add(x, y):
+    return x + y
+def substract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero!"
+    return x / y
+
+while True:
+    operation = input("Enter operation (+, -, *, /) or type 'exit': ").lower()
+    if operation == 'exit':
+        break
+    num1 = float(input('Please, enter the first number: '))
+    num2 = float(input('Please, enter the second number'))
+    if operation == '+':
+        result = add(num1, num2)
+    elif operation == '-':
+        result = substract(num1, num2)
+    elif operation == '*':
+        result = multiply(num1, num2)
+    elif operation == '/':
+        result = divide(num1, num2)
+    elif operation =='exit':
+        break
+    else:
+        result = "Invalid operation!"
+    print("Result:", result)
+
+
